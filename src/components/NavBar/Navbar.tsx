ef33e9b47eec553d54e6ba58babb9b51b37852ee
Gabriel Ong <gabrielongxe@gmail.com>
2023-09-10 14:50:46 +0800

    update about and navbar styling

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -2,9 +2,9 @@
 
 import { userData } from '@/data/constants';
 import { useState } from 'react';
-import { NavbarTitle } from './NavBarTitle';
 import { IoMdMenu, IoMdClose } from 'react-icons/io';
 import ThemeButton from '../UI/Card/Buttons/LikeButton/ThemeButton';
+import Link from 'next/link';
 
 type NavItem = {
   label: string;
@@ -25,6 +25,23 @@ const NAV_ITEMS: Array<NavItem> = [
     page: '/projects',
   },
 ];
+
+const NavbarTitle = () => {
+  return (
+    <div className='md:py-5 md:block'>
+      <div className='flex flex-col'>
+        <Link href='/'>
+          <h1 className='font-semibold text-xl dark:text-gray-100'>
+            {userData.name}
+          </h1>
+          <p className='text-base font-light text-gray-500 dark:text-gray-300'>
+            {userData.designation}
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 const SocialMediaButtons = () => {
   return (
@@ -63,25 +80,48 @@ const SocialMediaButtons = () => {
 export const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
 
+  const HamburgerMenu = () => {
+    return (
+      <div className='md:hidden'>
+        <button
+          className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border'
+          onClick={() => setNavbar(!navbar)}
+        >
+          {navbar ? <IoMdClose size={25} /> : <IoMdMenu size={25} />}
+        </button>
+      </div>
+    );
+  };
+
   return (
-    <div className='max-w-6xl  mx-auto px-4 py-10 md:py-2 bg-gray-50 dark:bg-stone-900'>
-      <div className='flex  md:flex-row justify-between items-center'>
-        <NavbarTitle />
+    <header className='max-w-6xl  mx-auto px-4 sm:px-20 py-10 md:py-2 bg-gray-50 dark:bg-stone-900'>
+      <div className='justify-between md:items-center md:flex'>
         <div>
-          <button onClick={() => setNavbar(!navbar)}>
-            {navbar ? <IoMdClose size={25} /> : <IoMdMenu size={25} />}
-          </button>
+          <div className='flex flex-row items-center justify-between py-3 md:py-5 md:block'>
+            <NavbarTitle />
+            <HamburgerMenu />
+          </div>
         </div>
-        <div className='space-x-8 md:block'>
-          {NAV_ITEMS.map((item) => {
-            return <a key={item.label}>{item.label}</a>;
-          })}
-        </div>
-        <div className='space-x-4 flex flex-row items-center'>
-          <SocialMediaButtons />
-          <ThemeButton />
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? 'block' : 'hidden'
+            }`}
+          >
+            <div className='flex  flex-row'>
+              <div className='flex flex-row items-center space-x-5'>
+                {NAV_ITEMS.map((item) => {
+                  return <a key={item.label}>{item.label}</a>;
+                })}
+              </div>
+              <div className='flex flex-row items-center space-x-4'>
+                <SocialMediaButtons />
+                <ThemeButton />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </header>
   );
 };
