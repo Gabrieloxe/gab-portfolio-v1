ccc72411aea02cb9d62ececfb2be783e85223fce
Gabriel Ong <gabrielongxe@gmail.com>
2023-10-07 17:11:57 +0900

    Experience page section initialize

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,11 @@
+
+const Experience = () => {
+  return (
+    <div className='px-2 flex mx-auto'>
+      <h1>Experience Page section</h1>
+      <div> </div>
+    </div>
+  );
+};
+
+export default Experience;
